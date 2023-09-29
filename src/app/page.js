1459740057b7673543e85b4e737f9c77d7fa66b1
Hrithik <slumdog4321@gmail.com>
2023-09-29 17:56:10 +0530

    menu Component added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu/Menu";
+
 export default function Home() {
-  return <h1 className="font-bold text-4xl ">Welcome to real Time Sketch board</h1>;
+  return <Menu />;
 }
